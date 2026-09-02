7bcc43d64e539cc1a9be5e577868a98a225f4eaf
Adrian Silva Santos Lima <adrian.silva@dinfor.uniceub>
2026-09-02 09:23:02 -0300

    fix: remove chave sensível exposta no código

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,9 +6,6 @@ function subtrair(a, b) {
   return a - b;
 }
 
-// SIMULAÇÃO DE VAZAMENTO DE SEGREDO
-const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE1234567890abcdef";
-
 module.exports = {
   somar,
   subtrair
