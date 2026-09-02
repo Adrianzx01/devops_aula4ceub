cf31fa2b840b0caf345686e962fcd6cae88cb501
Adrian Silva Santos Lima <adrian.silva@dinfor.uniceub>
2026-09-02 09:21:06 -0300

    test: adiciona chave de API para teste de segredos

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,6 +6,9 @@ function subtrair(a, b) {
   return a - b;
 }
 
+// SIMULAÇÃO DE VAZAMENTO DE SEGREDO
+const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE1234567890abcdef";
+
 module.exports = {
   somar,
   subtrair
